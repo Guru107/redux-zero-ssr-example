f6ecd5a38de7b5fee6188b661ff8c7d273dca31d
Guru107 <guruak107@gmail.com>
2017-10-29 13:58:26 +0530

    added paths

---
+++ webpack/paths.js
@@ -0,0 +1,10 @@
+const { join,sep } = require('path')
+const pathJoin = p => join(__dirname,'..',p)
+
+module.exports = {
+	NODE_MODULES: pathJoin('node_modules'),
+	CLIENT: pathJoin(`src${sep}client`),
+	SERVER: pathJoin(`src${sep}server`),
+	SRC: pathJoin('src'),
+	APP: pathJoin(`src${sep}app`)
+}
